42f3db939212fd75e7441ad2906a80f881efe4f0
Bojan Ristovski <bojan@angelis.agency>
2024-02-05 09:24:52 +0100

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -37,6 +37,9 @@
 //     { name: 'StaticKit', url: 'https://tailwindui.com/img/logos/statickit-logo-gray-400.svg' },
 //     { name: 'Workcation', url: 'https://tailwindui.com/img/logos/workcation-logo-gray-400.svg' },
 // ]
+import logoImg from "./cer-logo-white.png"
+import footerImg from "./footer_img.png"
+
 const footerNavigation = {
     // main: [
     //     { name: 'About', href: '#' },
@@ -48,8 +51,9 @@ const footerNavigation = {
     // ],
     social: [
         {
-            name: 'Facebook',
+            name: 'facebook',
             href: 'https://www.facebook.com/iaestecer',
+            mask: "IAESTE Central European Region",
             icon: (props) => (
                 <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                     <path
@@ -61,8 +65,9 @@ const footerNavigation = {
             ),
         },
         {
-            name: 'Instagram',
+            name: 'instagram',
             href: 'https://www.instagram.com/iaestecer.official/',
+            mask: "@iaestecer.official",
             icon: (props) => (
                 <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                     <path
@@ -73,43 +78,59 @@ const footerNavigation = {
                 </svg>
             ),
         },
-        
+        {
+            name: 'website',
+            href: 'https://cer-core.iaeste.org/',
+            mask: "https://cer-core.iaeste.org/",
+            icon: (props) => (
+                <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                    <path
+                        fillRule="evenodd"
+                        d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            ),
+        },
+        {
+            name: 'email',
+            // href: 'https://www.instagram.com/iaestecer.official/',
+            mask: "cer-management@iaeste.org",
+            icon: (props) => (
+                <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                    <path
+                        fillRule="evenodd"
+                        d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            ),
+        },
     ],
 }
 
 export default function Footer() {
     return (
-        <div className="bg-white">
-            {/* Footer section */}
-            <footer className="mt-24 bg-gray-900 sm:mt-12">
-                <div className="mx-auto max-w-md overflow-hidden py-12 px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-                    {/* <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-                        {footerNavigation.main.map((item) => (
-                            <div key={item.name} className="px-5 py-2">
-                                <a href={item.href} className="text-base text-gray-400 hover:text-gray-300">
-                                    {item.name}
-                                </a>
-                            </div>
-                        ))}
-                    </nav> */}
-                    <div className="mt-8 flex justify-center space-x-6">
-                        {footerNavigation.social.map((item) => (
-                            // eslint-disable-next-line react/jsx-no-target-blank
-                            <a key={item.name} href={item.href} target="_blank" className="text-gray-400 hover:text-gray-300">
-                                <span className="sr-only">{item.name}</span>
-                                <item.icon className="h-6 w-6" aria-hidden="true" />
-                            </a>
-                        ))}
-                    </div>
-                    <p className="mt-8 text-center text-base text-gray-400">
-                        Visit our website: <a href="https://cer-core.iaeste.org/ ">
-                            cer-core.iaeste.org</a>
-                    </p>
-                    <p className="mt-8 text-center text-base text-gray-400">
-                        &copy;IAESTE Central European Region
-                    </p>
+        <footer className="bg-gray-900" style={{ backgroundColor: "#0A3D58" }}>
+        <div className="mx-auto max-w-md overflow-hidden px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-20">
+                <div className="mb-1 sm:mb-0">
+                    <img src={logoImg} alt="" />
                 </div>
-            </footer>
+                <div className="flex flex-col justify-start pt-4">
+                    {footerNavigation.social.map((item) => (
+                        // eslint-disable-next-line react/jsx-no-target-blank
+                        <a key={item.name} href={item.href ?? '#'} target="_blank" className="text-gray-400 hover:text-gray-300 flex flex-row justify-between">
+                            <span className="font-semibold text-white">{item.name}</span>
+                            <span className="">{item.mask}</span>
+                        </a>
+                    ))}
+                </div>
+                <div className="hidden lg:block">
+                    <img src={footerImg} alt="" />
+                </div>
+            </div>
         </div>
+    </footer>
     )
 }
