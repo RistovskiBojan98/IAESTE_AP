dabd578fa6dcd1a89292d4bc2f30c12adaba170c
Bojan Ristovski <ristovski.bojan98@gmail.com>
2024-05-22 15:38:42 +0200

    Update filter

--- src/components/sr-weekends/EventFilter/Filter.jsx
+++ src/components/sr-weekends/EventFilter/Filter.jsx
@@ -99,13 +99,13 @@ const FilterPopup = ({ onClose, events }) => {
                     <i className="fa-solid fa-x"></i>
                 </button>
                 <div className="w-full bg-[#0B3D59] p-2 md:p-4 text-white flex flex-col justify-between">
-                    <h3 className="text-2xl md:text-4xl font-bold mb-1 md:mb-4">Filter Events</h3>
-                    <h3 className="text-sm">Select the properties to filter the events</h3>
-                    <hr className='mt-4'></hr>
+                    <h3 className="text-2xl md:text-4xl font-bold mb-1 md:mb-2">Filter Events</h3>
+                    <h3 className="text-base">Select the properties to filter the events</h3>
+                    <hr className='mt-2'></hr>
                     <div className={css.overflow}>
-                        <div className="my-2 md:my-4">
+                        <div className="my-2">
                             <div className='flex flex-row w-full justify-start items-center gap-1'>
-                                <i className='fas fa-calendar-alt text-lg md:text-2xl px-2 py-1' />
+                                <i className='fas fa-globe text-white text-lg md:text-2xl px-2 py-1' />
                                 <h4 className="text-lg md:text-2xl font-bold">Countries:</h4>
                             </div>
                             {countries.map((country, index) => (
