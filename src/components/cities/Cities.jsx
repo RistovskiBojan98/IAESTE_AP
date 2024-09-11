568f3f1d800ca9f578f61bde34c66be3ad8e13dd
RistovskiBojan98 <ristovski.bojan98@gmail.com>
2024-09-11 14:22:36 +0200

    Update components

--- src/components/cities/Cities.jsx
+++ src/components/cities/Cities.jsx
@@ -15,7 +15,6 @@ const Cities = ({ country, citiesRef }) => {
   const additionalCities = totalCities > 5 ? countryCities.cities.slice(5, totalCities) : 0
 
   return (
-    <>
       <section ref={citiesRef}>
         <div className={`${css.container} bg-gradient-to-r from-[#1B75BB] via-[#27A9E1] to-[#49C0B5] shadow-xl sm:overflow-hidden sm:rounded-2xl mb-10`}>
           <div className="mx-auto max-w-3xl divide-y-2 divide-gray-200">
@@ -23,9 +22,8 @@ const Cities = ({ country, citiesRef }) => {
               Places that we recommend visiting:
             </h2>
             <dl className="mt-6 space-y-6 divide-y divide-[#F1F1E6]">
-
-              {firstFiveCities.map((faq) => (
-                <Disclosure as="div" key={faq.question} className="pt-6 text-start">
+              {firstFiveCities.map((faq, index) => (
+                <Disclosure as="div" key={index} className="pt-6 text-start">
                   {({ open }) => (
                     <>
                       <dt className="text-[#F1F1E6] hover:text-[#B2D8FB]">
@@ -55,67 +53,59 @@ const Cities = ({ country, citiesRef }) => {
 
               {additionalCities && (
                 <Disclosure as="div" key="additionalCities">
-                  {() => (
-                    <>
-                      <Disclosure.Panel as="dt" className="space-y-6 divide-y divide-[#F1F1E6]" key="additionalCitiesPanel">
-                        {countryCities.cities.map((faq, index) => {
-                          if (index >= 5) {
-                            return <Disclosure as="div" key={faq.question} className="pt-6 text-start">
-                              {({ open2 }) => (
-                                <>
-                                  <div className="text-[#F1F1E6] hover:text-[#B2D8FB]">
-                                    <Disclosure.Button
-                                      className="flex w-full items-start justify-between text-left px-3">
-                                      <span className="font-semibold text-2xl">
-                                        {faq.question}
-                                      </span>
-                                      <span className="ml-6 flex h-7 items-center">
-                                        <ChevronDownIcon
-                                          className={classNames(
-                                            open2 ? "-rotate-180" : "rotate-0",
-                                            "h-6 w-6 transform"
-                                          )}
-                                          aria-hidden="true"
-                                        />
-                                      </span>
-                                    </Disclosure.Button>
-                                  </div>
-                                  <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                                    <p className="pt-6 text-lg text-[#F1F1E6]">{faq.answer}</p>
-                                  </Disclosure.Panel>
-                                </>
-                              )}
-                            </Disclosure>;
-                          }
-                          return <></>;
-                        })}
-                      </Disclosure.Panel>
-                      <dt className="text-lg">
-                        <Disclosure.Button className="flex md:w-2/7 items-start justify-between text-left text-[#F1F1E6] hover:text-[#B2D8FB] pt-6"
-                          onClick={onOpenCitiesHandler} style={{ fontSize: '18px' }}>
-                          <span className="font-base">
-                            {isOpen ? 'Show less' : 'Show more'}
-                          </span>
-                          <span className="ml-6 flex h-7 items-center">
-                            <ChevronDownIcon
-                              className={classNames(
-                                isOpen ? "-rotate-180" : "rotate-0",
-                                "h-6 w-6 transform"
-                              )}
-                              aria-hidden="true"
-                            />
-                          </span>
-                        </Disclosure.Button>
-                      </dt>
-                    </>
-                  )}
+                  <Disclosure.Panel as="dt" className="space-y-6 divide-y divide-[#F1F1E6]" key="additionalCitiesPanel">
+                    {additionalCities?.map((faq, index) =>
+                        <Disclosure as="div" key={index} className="pt-6 text-start">
+                          {({ open2 }) => (
+                            <>
+                              <div className="text-[#F1F1E6] hover:text-[#B2D8FB]">
+                                <Disclosure.Button
+                                  className="flex w-full items-start justify-between text-left px-3">
+                                  <span className="font-semibold text-2xl">
+                                    {faq.question}
+                                  </span>
+                                  <span className="ml-6 flex h-7 items-center">
+                                    <ChevronDownIcon
+                                      className={classNames(
+                                        open2 ? "-rotate-180" : "rotate-0",
+                                        "h-6 w-6 transform"
+                                      )}
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                </Disclosure.Button>
+                              </div>
+                              <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                                <p className="pt-6 text-lg text-[#F1F1E6]">{faq.answer}</p>
+                              </Disclosure.Panel>
+                            </>
+                          )}
+                        </Disclosure>
+                    )}
+                  </Disclosure.Panel>
+                  <dt className="text-lg">
+                    <Disclosure.Button className="flex md:w-2/7 items-start justify-between text-left text-[#F1F1E6] hover:text-[#B2D8FB] pt-6"
+                      onClick={onOpenCitiesHandler} style={{ fontSize: '18px' }}>
+                      <span className="font-base">
+                        {isOpen ? 'Show less' : 'Show more'}
+                      </span>
+                      <span className="ml-6 flex h-7 items-center">
+                        <ChevronDownIcon
+                          className={classNames(
+                            isOpen ? "-rotate-180" : "rotate-0",
+                            "h-6 w-6 transform"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Disclosure.Button>
+                  </dt>
                 </Disclosure>
               )}
             </dl>
           </div>
         </div>
       </section>
-    </>
   );
 };
 
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { countries } from "./countries";
-import classes from "./Countries.module.css";
 
 const Countries = ({ passRef }) => {
   const [displayedCountries, setDisplayedCountries] = useState(countries);
@@ -18,47 +17,40 @@ const Countries = ({ passRef }) => {
 
   return (
     <div className="bg-white" ref={passRef} id="countries-div" style={{ overflowX: 'hidden' }}>
-      <div className="mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto py-8 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div>
-          <h2
-            className="font-bold"
-            style={{ fontSize: "1.5rem", fontWeight: "600" }}
-          >
+          <h2 className="font-bold text-3xl">
             IAESTE CER & CoRe Member Countries
           </h2>
           <input
             type="text"
+            className="p-2 mt-4 max-w-md w-full border-2"
             placeholder="Search for a country"
             onChange={onFilterCountriesHandler}
-            style={{
-              padding: "0.5rem",
-              paddingLeft: "1rem",
-              border: "1px solid black",
-              borderRadius: "10px",
-              width: "100%",
-              maxWidth: "400px",
-              marginTop: "1rem",
-            }}
+            style={{ border: "1px solid black", borderRadius: "10px" }}
           />
         </div>
         <div className="mt-12 grid grid-cols-2 gap-y-20 gap-x-6 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
           {displayedCountries.map((country, index) => (
             <div className="text-center" key={index}>
-              <a key={country.id}
-                className="group items-center"
-                href={country.href}>
+              <a key={country.id} className="group items-center" href={country.href}>
                   <div className="mx-auto h-20 w-20 sm:h-40 sm:w-40">
-                  <img
-                    src={country.imageSrc}
-                    alt={country.imageAlt}
-                    className={`group-hover:opacity-75 rounded-full ${classes.imgHover}`}
-                    style={{width:'100%', height:'100%'}}
-                  />
+                    <img
+                      src={country.imageSrc}
+                      alt={country.imageAlt}
+                      className="group-hover:opacity-75 rounded-full hover:scale-110 w-full h-full"
+                    />
                 </div>
               </a>
-              <a href={country.href}><h1 className="my-5 font-semibold text-xl text-[#0B3D59] hover:text-sky-700">IAESTE {country.name}</h1></a>
-              <button className="btn text-center text-sm w-1/2 sm:w-1/3 py-2 max-w-20 hover:bg-gradient-to-r from-[#1B75BB] via-[#27A9E1] to-[#49C0B5]" style={{ backgroundColor: '#0B3D59', color: 'white', borderRadius: '19px' }}>
-                <a href={country.pdf} target='_blank' rel="noreferrer"><i className="fa-solid fa-download text-white text-lg mr-2"></i>PDF</a>
+              <a href={country.href}>
+                <h1 className="my-5 font-semibold text-xl text-[#0B3D59] hover:text-sky-700">
+                  IAESTE {country.name}
+                </h1>
+              </a>
+              <button className="btn text-center text-white text-sm w-1/2 sm:w-1/3 py-2 max-w-20 bg-[#0B3D59] hover:bg-gradient-to-r from-[#1B75BB] via-[#27A9E1] to-[#49C0B5] rounded-2xl">
+                <a href={country.pdf} target='_blank' rel="noreferrer">
+                  <i className="fa-solid fa-download text-white text-lg mr-2"></i>PDF
+                  </a>
               </button>
             </div>
           ))}
