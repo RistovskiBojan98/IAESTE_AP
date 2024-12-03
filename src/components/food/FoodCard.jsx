397061d00911ab308cc8db4380b9821b76d6be1a
RistovskiBojan98 <ristovski.bojan98@gmail.com>
2024-12-03 09:08:58 +0100

    Update food card

--- src/components/food/FoodCard.jsx
+++ src/components/food/FoodCard.jsx
@@ -3,8 +3,11 @@ import css from "../app.module.css";
 
 const FoodCard = ({ title, description }) =>
   <div className={css.cardContainer}>
-    <h3 className="border-b ">{title}</h3>
-    <p>{description}</p>
+    <h3 className="pt-2">{title}</h3>
+    { !!description && 
+      <p className="border-t pt-5">{description}</p>
+    }
+    
   </div>
 
 export default FoodCard;
